9fdc3265b07721a447017dc807ef3aa2cad918c2
Soobin Kim <sharsta5@gmail.com>
2017-03-18 22:54:12 +0900

    Fixed UI

--- RealEstate/FindView.Designer.cs
+++ RealEstate/FindView.Designer.cs
@@ -62,7 +62,8 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Name = "FindView";
-            this.Text = "findtest";
+            this.ShowIcon = false;
+            this.Text = "찾기";
             this.Load += new System.EventHandler(this.FindView_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
